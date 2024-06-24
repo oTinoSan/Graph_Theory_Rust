69633fa71d4c82b0a71179f10776568622bafee9
otinosan <santinosini@gmail.com>
2024-06-24 12:03:32 -0400

    adds compressed_sparse_rows.rs. runs.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use array2d::Array2D;
-use graph_theory::*;
+use graph_theo::compressed_sparse_rows::CompressedSparseRows;
 
 fn main() {
     // Define the adjacency matrix for the given graph
-    let rows = vec![
+    let rows: Vec<Vec<u64>> = vec![
         vec![0, 1, 1, 1, 0],
         vec![0, 0, 1, 1, 1],
         vec![1, 0, 0, 1, 0],
@@ -11,23 +11,12 @@ fn main() {
         vec![1, 0, 0, 0, 0],
     ];
 
-    let adj = Array2D::from_rows(&rows).unwrap();
+    let converted_array = Array2D::from_rows(&rows).expect("Failed?");
 
-    println!("Starting adjacency matrix: {:?}", adj.as_rows());
+    // Convert the adjacency matrix to CSR format
+    let compression = CompressedSparseRows::from_adjacency(&converted_array);
 
-    let csr = CompressedSparseRows::from_adjacency_matrix(adj);
-
-    println!("Converted to CSR: {:?}", csr);
-
-    let adj = csr.to_adjacency_matrix();
-
-    println!("Back to adjacency matrix: {:?}", adj.as_rows());
-
-    let edges = edge_list_from_file("edges.txt");
-
-    println!("Edges: {:?}", edges);
-
-    let csr = CompressedSparseRows::from_edge_list(edges);
-
-    println!("Converted to CSR: {:?}", csr);
+    // Print the CSR representation
+    println!("Row Offset: {:?}", compression.row_offset);
+    println!("Column Indices: {:?}", compression.col_indices);
 }
