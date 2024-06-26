f2e3145c29fef8041af46116a15d07935ed19d2a
Ethan Tenney <ebtenne@lps.umd.edu>
2024-06-26 11:46:40 -0400

    fixed csr implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
 use array2d::Array2D;
 
-struct CompressedSparseRows {
+#[derive(Debug, Clone)]
+pub struct CompressedSparseRows {
     col_indices: Vec<u64>,
     row_offsets: Vec<u64>,
 }
 
 impl CompressedSparseRows {
-    fn from_adjacency_matrix(array: Array2D<u64>) -> Self {
+    pub fn from_adjacency_matrix(array: Array2D<u64>) -> Self {
         let mut col_indices = Vec::new();
         let mut row_offsets = Vec::new();
         let mut count = 0;
@@ -15,20 +16,16 @@ impl CompressedSparseRows {
             for j in 0..array.column_len() {
                 if *array.get(i, j).unwrap() == 1 {
                     col_indices.push(j as u64);
+                    count = count + 1;
                 }
-                count = count + 1;
+                
             }
         }
+        row_offsets.push(count); // push index of the end of the array
 
         CompressedSparseRows {
             col_indices,
             row_offsets,
         }
     }
-}
-pub fn compressed_sparse_rows(array: Array2D<u32>) -> (Vec<u32>, Vec<u32>) {
-    
-    
-
-    todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use array2d::Array2D;
 use graph_exploration::*;
 
 fn main() {
-    println!("Hello, world!");
+    let array: Array2D<u64> = Array2D::from_rows(
+        &vec![vec![0, 1, 1, 1, 0],
+        vec![0, 0, 1, 1, 1],
+        vec![1, 0, 0, 1, 0],
+        vec![0, 0, 1, 0, 1],
+        vec![1, 0, 0, 0, 0]
+        ]
+    ).unwrap();
+
+    let result = graph_exploration::CompressedSparseRows::from_adjacency_matrix(array);
+
+    println!("{:?}", result);
 }
