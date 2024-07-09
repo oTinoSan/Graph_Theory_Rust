de59246e5c3720f7602973083f5721ed262f7072
otinosan <santinosini@gmail.com>
2024-07-09 14:33:28 -0400

    makes active messaging public

--- src/lamellar_exercise/active_messaging_example.rs
+++ src/lamellar_exercise/active_messaging_example.rs
@@ -17,7 +17,7 @@ impl LamellarAM for HelloWorld {
     }
 }
 
-fn active_example (){
+pub fn active_example (){
     let mut world = lamellar::LamellarWorldBuilder::new().build();
     let my_pe = world.my_pe();
     let num_pes = world.num_pes();
