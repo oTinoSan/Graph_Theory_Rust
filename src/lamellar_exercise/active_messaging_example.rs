254bcf4957aeb70d26e7a672a20f76f248cedf7e
otinosan <santinosini@gmail.com>
2024-07-09 15:14:26 -0400

    renames hello_world am

--- src/lamellar_exercise/active_messaging_example.rs
+++ src/lamellar_exercise/active_messaging_example.rs
@@ -1,12 +1,12 @@
 use lamellar::active_messaging::prelude::*;
 
 #[AmData(Debug, Clone)] // `AmData` is a macro used in place of `derive` 
-struct HelloWorld { //the "input data" we are sending with our active message
+struct HelloWorld_am { //the "input data" we are sending with our active message
     my_pe: usize, // "pe" is processing element == a node
 }
 
 #[lamellar::am] // at a highlevel registers this LamellarAM implemenatation with the runtime for remote execution
-impl LamellarAM for HelloWorld {
+impl LamellarAM for HelloWorld_am {
     async fn exec(&self) {
         println!(
             "Hello pe {:?} of {:?}, I'm pe {:?}",
@@ -21,7 +21,7 @@ pub fn active_example (){
     let mut world = lamellar::LamellarWorldBuilder::new().build();
     let my_pe = world.my_pe();
     let num_pes = world.num_pes();
-    let am = HelloWorld { my_pe: my_pe };
+    let am = HelloWorld_am { my_pe: my_pe };
     for pe in 0..num_pes{
         world.exec_am_pe(pe,am.clone()); // explicitly launch on each PE
     }
