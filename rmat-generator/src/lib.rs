ed0ecec3599226bbcdc54521b1414c87dfa299ba
Will Shayne <wwshayn@lps.umd.edu>
2024-07-11 11:24:02 -0400

    changed rmat generator to make a reproducible graph

--- rmat-generator/src/lib.rs
+++ rmat-generator/src/lib.rs
@@ -1,20 +1,23 @@
-use rand::{distributions::Distribution, thread_rng};
+use rand::{distributions::Distribution, rngs::SmallRng, thread_rng, RngCore, SeedableRng};
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Edge(pub usize, pub usize);
 
 #[derive(Clone, Debug)]
 pub struct RMATGraph {
     pub order: usize,
     fuzz: f64,
+    seed: u64,
+    gen: SmallRng,
+    edge_count: usize,
     partition: [f64; 4],
     directed: bool,
-    pub edges: Vec<Edge>,
 }
 
 impl RMATGraph {
-    pub fn new(order: usize, fuzz: f64, partition: [f64; 4], directed: bool) -> Self {
-        Self {order, fuzz, partition, directed, edges: vec![]}
+    pub fn new(order: usize, fuzz: f64, seed: Option<u64>, edge_count: usize, partition: [f64; 4], directed: bool) -> Self {
+        let seed = seed.unwrap_or_else(|| thread_rng().next_u64());
+        Self {order, fuzz, seed, gen: SmallRng::seed_from_u64(seed), edge_count, partition, directed}
     }
 
     pub fn generate_edge(&mut self) -> Edge {
@@ -25,13 +28,13 @@ impl RMATGraph {
         let mut step = 1 << (self.order - 1);
         let mut u = 0;
         let mut v = 0;
-        let distribution = rand::distributions::Uniform::new_inclusive(0.0, 1.0);
+        let distribution = rand::distributions::Uniform::new(0.0, 1.0);
         for _ in 0..self.order {
             // add noise at each step, then normalize
-            a *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut thread_rng());
-            b *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut thread_rng());
-            c *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut thread_rng());
-            d *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut thread_rng());
+            a *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut self.gen);
+            b *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut self.gen);
+            c *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut self.gen);
+            d *= 1. - self.fuzz + 2. * self.fuzz * distribution.sample(&mut self.gen);
 
             let s = a + b + c + d;
             a /= s;
@@ -40,7 +43,7 @@ impl RMATGraph {
             // ensure the probabilities add to 1
             d = 1. - a - b - c;
 
-            let p = distribution.sample(&mut thread_rng());
+            let p = distribution.sample(&mut self.gen);
             if p < a {}
             else if p < a + b {
                 u += step;
@@ -52,11 +55,44 @@ impl RMATGraph {
             }
             step >>= 1;
         }
-        self.edges.push(Edge(u, v));
         if !self.directed {
-            self.edges.push(Edge(v, u));
             return Edge(usize::min(u, v), usize::max(u, v));
         }
         Edge(u, v)
+    }
+}
+
+impl IntoIterator for RMATGraph {
+    type Item = Edge;
+    type IntoIter = RMATIter;
+    fn into_iter(mut self) -> Self::IntoIter {
+        self.gen = SmallRng::seed_from_u64(self.seed);
+        RMATIter {graph: self, next_edge: None, count: 0}
+    }
+}
+
+pub struct RMATIter {
+    graph: RMATGraph,
+    next_edge: Option<Edge>,
+    count: usize
+}
+
+impl Iterator for RMATIter {
+    type Item = Edge;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count > self.graph.edge_count {
+            return None;
+        }
+        self.count += 1;
+        if let Some(e) = self.next_edge {
+            self.next_edge = None;
+            Some(e)
+        } else if self.graph.directed {
+            Some(self.graph.generate_edge())
+        } else {
+            let Edge(u, v) = self.graph.generate_edge();
+            self.next_edge = Some(Edge(v, u));
+            Some(Edge(u, v))
+        }
     }
 }
--- rmat-generator/src/main.rs
+++ rmat-generator/src/main.rs
@@ -1,9 +1,9 @@
 use rmat_generator::RMATGraph;
 
 fn main() {
-    let mut g = RMATGraph::new(18, 0.1, [0.57, 0.19, 0.19, 0.05], false);
-    for _ in 0..1000000 {
-        g.generate_edge();
-    }
-    println!("{:?}", g);
+    let g = RMATGraph::new(18, 0.1, None, 100000, [0.57, 0.19, 0.19, 0.05], false);
+    let edges: Vec<_> = g.clone().into_iter().collect();
+    let edges_2: Vec<_> = g.clone().into_iter().collect();
+    assert_eq!(edges, edges_2);
+    println!("{:?}", edges);
 }
