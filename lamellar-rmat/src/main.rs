b2825820ed934f0b59f770ddc25301006ce02738
Will Shayne <wwshayn@lps.umd.edu>
2024-07-12 14:00:09 -0400

    testing

--- lamellar-rmat/src/main.rs
+++ lamellar-rmat/src/main.rs
@@ -1,6 +1,6 @@
 use lamellar_rmat::DistRMAT;
 use rand::rngs::SmallRng;
-use lamellar::{darc::Darc, typed_am_group, ActiveMessaging};
+use lamellar::{darc::Darc, ActiveMessaging};
 use std::io::prelude::*;
 
 #[lamellar::AmData(Clone)]
@@ -21,11 +21,12 @@ fn main() {
     let my_pe = world.my_pe();
     let num_pes = world.num_pes();
 
-    let gen = DistRMAT::<SmallRng>::new(&world, 18, 0.1, Some(0), 100000, [0.57, 0.19, 0.19, 0.05], false);
+    let gen = DistRMAT::<SmallRng>::new(&world, 18, 0.1, Some(0), 7000000, [0.57, 0.19, 0.19, 0.05], false);
     let gen = Darc::new(&world, gen).unwrap();
     if my_pe == 0 {
         let edges: Vec<_> = world.block_on(world.exec_am_all(Yoink {gen})).into_iter().flatten().collect();
-        let f = std::fs::File::options().write(true).truncate(true).open("graph.json").unwrap();
+        println!("Found {} total edges at pe {}", edges.len(), my_pe);
+        let f = std::fs::File::options().write(true).truncate(true).create(true).open("graph.json").unwrap();
         let mut writer = std::io::BufWriter::new(f);
         serde_json::to_writer_pretty(&mut writer, &edges).unwrap();
         writer.flush();
