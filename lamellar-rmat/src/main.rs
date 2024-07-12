5f74556c9876197f723c2320459d3ce133d027d4
Will Shayne <wwshayn@lps.umd.edu>
2024-07-12 13:37:05 -0400

    added testing binary for distributed rmat generator

---
+++ lamellar-rmat/src/main.rs
@@ -0,0 +1,33 @@
+use lamellar_rmat::DistRMAT;
+use rand::rngs::SmallRng;
+use lamellar::{darc::Darc, typed_am_group, ActiveMessaging};
+use std::io::prelude::*;
+
+#[lamellar::AmData(Clone)]
+struct Yoink {
+    gen: Darc<DistRMAT<SmallRng>>
+}
+
+#[lamellar::am]
+impl LamellarAm for Yoink {
+    async fn exec(&self) -> Vec<dist_structs::Edge> {
+        self.gen.iter().collect::<Vec<_>>()
+    }
+}
+
+
+fn main() {
+    let world = lamellar::LamellarWorldBuilder::new().build();
+    let my_pe = world.my_pe();
+    let num_pes = world.num_pes();
+
+    let gen = DistRMAT::<SmallRng>::new(&world, 18, 0.1, Some(0), 100000, [0.57, 0.19, 0.19, 0.05], false);
+    let gen = Darc::new(&world, gen).unwrap();
+    if my_pe == 0 {
+        let edges: Vec<_> = world.block_on(world.exec_am_all(Yoink {gen})).into_iter().flatten().collect();
+        let f = std::fs::File::options().write(true).truncate(true).open("graph.json").unwrap();
+        let mut writer = std::io::BufWriter::new(f);
+        serde_json::to_writer_pretty(&mut writer, &edges).unwrap();
+        writer.flush();
+    }
+}
