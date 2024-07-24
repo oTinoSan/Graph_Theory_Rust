a89b3aa6c13d1b27745408f7d55eccf66164415c
otinosan <santinosini@gmail.com>
2024-07-24 14:18:59 -0400

    adds dist_map binary

--- sssp_delta/src/bin/func_dist_map.rs
+++ sssp_delta/src/bin/func_dist_map.rs
@@ -1,13 +1,132 @@
 use lamellar::active_messaging::prelude::*;
 use lamellar::darc::prelude::*;
-use std::env::{self, args};
-use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Mutex;
-use std::time::Instant;
-use std::collections::{HashMap, HashSet};
-use rayon::prelude::*;
-use sssp_delta::dist_hash_set::*;
-use sssp_delta::dist_hash_map::*;
+use serde::{Deserialize, Serialize};
+use lamellar::LamellarTeam;
+use std::collections::HashMap;
+use std::future::Future;
+use std::sync::Arc;
+
+// edge: (vertex, weight)
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct AdjList {
+    pub edges: Vec<(usize, f64)>,
+    pub tent: f32,
+}
+
+impl std::fmt::Display for AdjList {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "AdjList {{ vertex, weight: {:?}, tent: {} }}", self.edges, self.tent)
+    }
+}
+
+// data: <node, (tent, (vertex, weight)>
+#[derive(Clone, Debug)]
+pub struct DistHashMap {
+    num_pes: usize,
+    team: Arc<LamellarTeam>,
+    data: LocalRwDarc<HashMap<i32, AdjList>>, //unforunately we can't use generics here due to constraints imposed by ActiveMessages
+}
+
+impl DistHashMap {
+    pub fn new(world: &LamellarWorld,  num_pes: usize) -> Self {
+        let team = world.team();
+        DistHashMap {
+            num_pes,
+            team: team.clone(),
+            data: LocalRwDarc::new(team, HashMap::new()).unwrap(),
+        }
+    }
+
+    fn get_key_pe(&self, k: i32) -> usize {
+        k as usize % self.num_pes
+    }
+
+    pub fn add(&self, k: i32, v: AdjList) -> impl Future {
+        let dest_pe = self.get_key_pe(k);
+        self.team.exec_am_pe(
+            dest_pe,
+            DistHashMapOp {
+                data: self.data.clone(),
+                cmd: DistCmd::Add(k, v),
+            },
+        )
+    }
+
+    pub fn get(&self, k: i32) -> impl Future<Output = DistCmdResult> {
+        let dest_pe = self.get_key_pe(k);
+        self.team.exec_am_pe(
+            dest_pe,
+            DistHashMapOp {
+                data: self.data.clone(),
+                cmd: DistCmd::Get(k),
+            },
+        )
+    }
+
+    pub fn visit(&self, k: i32, v: f32) -> impl Future<Output = DistCmdResult> {
+        let dest_pe = self.get_key_pe(k);
+        self.team.exec_am_pe(
+            dest_pe,
+            DistHashMapOp {
+                data: self.data.clone(),
+                cmd: DistCmd::Visit(k, v),
+            },
+        )
+    }
+
+}
+
+// this is one way we can implement commands for the distributed hashmap
+// a maybe more efficient way to do this would be to create an individual
+// active message for each command
+// #[AmData(Debug, Clone)] eventually we will be able to do this... instead  derive serialize and deserialize directly with serde
+#[derive(Debug, Clone, Serialize, Deserialize)]
+enum DistCmd {
+    Add(i32, AdjList),
+    Get(i32),
+    Visit(i32, f32),
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub enum DistCmdResult {
+    Add,
+    Get(i32),
+    Visit(Option<AdjList>), 
+}
+
+#[AmData(Debug, Clone)]
+struct DistHashMapOp {
+    data: LocalRwDarc<HashMap<i32, AdjList>>, //unforunately we can't use generics here due to constraints imposed by ActiveMessages
+    cmd: DistCmd,
+}
+
+#[am]
+impl LamellarAM for DistHashMapOp {
+    async fn exec(self) -> DistCmdResult {
+        match &self.cmd {
+            DistCmd::Add(k, v) => {
+                self.data.write().await.insert(*k, v.clone());
+                DistCmdResult::Add
+            }
+            DistCmd::Get(k) => {
+                let data = self.data.read().await;
+                let v = data.get(&k).cloned();
+                println!("{:?}", v.unwrap());
+                DistCmdResult::Get(*k)
+            }
+            DistCmd::Visit(k, new_tent) => {
+                let mut data = self.data.write().await;
+                if let Some(adj_list) = data.get_mut(&k) {
+                    adj_list.tent = *new_tent;
+                    DistCmdResult::Visit(Some(adj_list.clone())) 
+                } else {
+                    DistCmdResult::Visit(None)
+                }
+            }
+        }
+    }
+}
+
 fn main() {
     let world = lamellar::LamellarWorldBuilder::new().build();
     let my_pe = world.my_pe();
@@ -53,4 +172,5 @@ fn main() {
     //         println!("Key does not exist")
     //     }
     // });
+
 }
