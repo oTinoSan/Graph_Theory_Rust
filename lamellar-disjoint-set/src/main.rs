35f887743cf1b847e02f5f69f5151f3ae1c6afcc
Will Shayne <wwshayn@lps.umd.edu>
2024-07-26 15:56:16 -0400

    changed testing settings

--- lamellar-disjoint-set/src/main.rs
+++ lamellar-disjoint-set/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let my_pe = world.my_pe();
     let num_pes = world.num_pes();
 
-    let disjoint_set = DisjointSet::new_with_vertices(&world, 100, 14);
+    let disjoint_set = DisjointSet::new_with_vertices(&world, 14, 14);
 
     let test_edges: Vec<Edge> = vec![(0, 1), (0, 3), (1, 2), (1, 5), (1, 10), (2, 4), (2, 5), (3, 4), (4, 5), (5, 7), (5, 10), (6, 8), (6, 9), (8, 11), (9, 11), (11, 12), (12, 13)].into_iter().map(|x| Edge::from(x)).collect();
 
